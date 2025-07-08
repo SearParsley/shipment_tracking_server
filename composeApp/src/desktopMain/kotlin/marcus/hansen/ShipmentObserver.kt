e80d7b3fed64d7ee487c9e36fb2148d139117003
SearParsley <161894009+SearParsley@users.noreply.github.com>
2025-07-08 09:53:57 -0600

    doc comments

--- composeApp/src/desktopMain/kotlin/marcus/hansen/ShipmentObserver.kt
+++ composeApp/src/desktopMain/kotlin/marcus/hansen/ShipmentObserver.kt
@@ -1,5 +1,11 @@
 package marcus.hansen
 
 interface ShipmentObserver {
+    /**
+     * Called by a Shipment (Subject) when its state changes.
+     * The observer should pull the necessary updated information from the provided shipment object.
+     *
+     * @param shipment The Shipment object whose state has changed.
+     */
     fun update(shipment: Shipment)
 }
--- composeApp/src/desktopMain/kotlin/marcus/hansen/UpdateStrategy.kt
+++ composeApp/src/desktopMain/kotlin/marcus/hansen/UpdateStrategy.kt
@@ -1,5 +1,12 @@
 package marcus.hansen
 
 interface UpdateStrategy {
+    /**
+     * Applies a specific type of update to a Shipment object.
+     * After applying the update, the strategy should call shipment.notifyObservers().
+     *
+     * @param shipment The Shipment object to be updated.
+     * @param updateData The data containing the details of the update.
+     */
     fun update(shipment: Shipment, update: ShippingUpdate)
 }
