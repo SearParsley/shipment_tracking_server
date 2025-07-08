af402a59d33b12a61d390b5f49942324838618a1
SearParsley <161894009+SearParsley@users.noreply.github.com>
2025-07-08 13:43:39 -0600

    Update NoteAddedStrategy.kt

--- composeApp/src/desktopMain/kotlin/marcus/hansen/NoteAddedStrategy.kt
+++ composeApp/src/desktopMain/kotlin/marcus/hansen/NoteAddedStrategy.kt
@@ -3,7 +3,7 @@ package marcus.hansen
 class NoteAddedStrategy : UpdateStrategy {
     override fun update(shipment: Shipment, update: ShippingUpdate) {
         if (update.otherInfo.isNotEmpty()) {
-            shipment.addNote(update.otherInfo[0]) // Using the public addNote method
+            shipment.addNote(update.otherInfo[0])
         }
         shipment.addUpdate(update)
         shipment.notifyObservers()
