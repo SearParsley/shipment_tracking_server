4dc7393f9498dc929ad5847161874740e1eedd59
SearParsley <161894009+SearParsley@users.noreply.github.com>
2025-07-22 13:02:00 -0600

    refactor

--- composeApp/src/desktopMain/kotlin/marcus/hansen/main.kt
+++ composeApp/src/desktopMain/kotlin/marcus/hansen/main.kt
@@ -2,21 +2,11 @@ package marcus.hansen
 
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.LaunchedEffect
 
 fun main() = application {
-    val trackingSimulator = remember { TrackingSimulator() }
+    val trackingServer = TrackingServer
 
-    LaunchedEffect(trackingSimulator) {
-        launch(Dispatchers.IO) {
-            trackingSimulator.runSimulation("updates.txt")
-        }
-    }
-
-    Window(onCloseRequest = ::exitApplication, title = "Shipment Tracking Simulator") {
-        UserInterface(trackingSimulator)
+    Window(onCloseRequest = ::exitApplication, title = "Shipment Tracking Server & Client") {
+        UserInterface(trackingServer)
     }
 }
